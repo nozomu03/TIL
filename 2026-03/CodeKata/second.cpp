a08a085276bcb9acfb117c21109627b140ffb7b8
saka4528 <nozomu466@gmail.com>
2026-03-09 09:06:38 +0900

    codekata

---
+++ 2026-03/CodeKata/second.cpp
@@ -0,0 +1,12 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num1, int num2) {
+    if(num1 >= 0 && num1 <= 100){
+        if(num2 >= 0 && num2 <= 100){
+            return num1 * num2;       
+        }   
+    }    
+}
