c797c20b4f4986ef27390bd4a6b04f48700ab962
saka4528 <nozomu466@gmail.com>
2026-03-18 09:16:07 +0900

    CodeKata

--- 2026-03/CodeKata/twonine.cpp
+++ 2026-03/CodeKata/twonine.cpp
@@ -1,14 +1,20 @@
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 
-int solution(vector<int> numbers) {
-    int answer = 45;
+vector<int> solution(vector<int> arr) {    
+	auto index = arr.begin();
 
-    for(int i = 0; i < numbers.size(); i++){
-        answer -= numbers[i];
+	for (auto i = arr.begin(); i < arr.end(); i++) {		
+		if (*index > *i) {
+			index = i;
+		}
+	}
+	arr.erase(index);
+    if(arr.size() == 0){
+        arr.push_back(-1);
     }
-    
-    return answer;
+    return arr;
 }
